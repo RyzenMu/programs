628a308fa5f9e965625734af9d58cb8c1eb7c1a2
RyzenMu <muruga13in2022@gmail.com>
2023-02-19 20:04:38 +0530

    random numbers js

---
+++ javaScript/foundations/Random/main.js
@@ -0,0 +1,16 @@
+let text = document.querySelector('h2').innerHTML;
+
+text = "Hello from JS";
+
+
+console.log(text);
+
+
+// Generating a random number using math MOdule
+
+randNum = Math.random();
+
+input = document.getElementById('input');
+
+input.innerHTML = randNum;
+
