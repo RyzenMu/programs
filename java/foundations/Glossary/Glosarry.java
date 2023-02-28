fc73bec0c64b935824f062389ea232065bb36a95
RyzenMu <muruga13in2022@gmail.com>
2023-02-28 05:50:05 +0530

    glossary java

---
+++ java/foundations/Glossary/Glosarry.java
@@ -0,0 +1,29 @@
+// package java.foundations.Glossary;
+
+public class Glosarry {
+
+    public static void main(String args){
+        /*
+         * API - Application programming interface
+         * AWT - Abstract Windowing Toolkit
+         * canvas - an applet component that can display graphics and text
+         * constructor - a method that is used to create an instantiation of a class
+         * deadlock - Deadlock occurs when two or more threads are awaiting for resources that they can not get.
+         * dynamic linking - when function called within a program are associated with the program at runtime.
+         * encapsulation
+         * final - a modifier that prevents subclass definition, makes variables constant, and prevents a subclass from overriding a method.
+         * Hotjava - a java capable browser from javaSoft
+         * interface
+         * javac - a command for running the java compiler
+         * javac_g
+         * JDBC - a database access Api
+         * runnable interface
+         * 
+         * 
+         * 
+         * 
+         * 
+         */
+    }
+    
+}
