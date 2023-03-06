51d1a491325b4734ed830ea1fc378014777d143f
RyzenMu <muruga13in2022@gmail.com>
2023-03-07 04:52:54 +0530

    History - Window Object

---
+++ javaScript/foundations/HistoryObject/main.js
@@ -0,0 +1,23 @@
+
+let his = window.history;
+
+console.log(his);
+
+console.log(his.length);
+
+// his.forward(); // forwards the history page
+
+let hist = history;
+
+console.log(hist);
+
+// forward() -> goes to next page
+// back() -> goes to previous page
+// go() -> goes to given number of the page
+
+
+//example
+
+
+// hist.forward();
+// hist.back();
