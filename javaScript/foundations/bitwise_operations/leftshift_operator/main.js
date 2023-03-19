e82211e32dde19d21e87d0059b8fd2ce1e127f65
RyzenMu <muruga13in2022@gmail.com>
2023-03-19 09:26:27 +0530

    bitwise right shift operator - js

---
+++ javaScript/foundations/bitwise_operations/leftshift_operator/main.js
@@ -0,0 +1,11 @@
+document.write("<h2 style=text-align:center;>Hello</h2>");
+
+let aVariable = 10; // binary = 1010
+
+let bVariable = aVariable << 2; //binary = 101000
+
+document.write(aVariable);
+
+document.write("<br>");
+
+document.write(bVariable);
