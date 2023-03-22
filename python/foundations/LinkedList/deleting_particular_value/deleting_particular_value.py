d76875eed6643c8b86cb6ef71152467ec81bfb6d
RyzenMu <muruga13in2022@gmail.com>
2023-03-23 05:19:54 +0530

    linked stack - python

--- python/foundations/LinkedList/deleting_particular_value/deleting_particular_value.py
+++ python/foundations/LinkedList/deleting_particular_value/deleting_particular_value.py
@@ -32,29 +32,27 @@ class LinkedList:
             del temp
             return value
 
-
-
 #Deleting a particular node from linked List
 
-def delVal(self, value):
-    if self.head is None:
-        print('Empty List')
-        return
-    if self.head.data == value:
-        temp = self.head
-        self.head = self.head.next
-        del temp
-        print('Value Deleted')
-    else:
-        current = self.head
-        prev = None
-        while current != None and current.data != value:
-            prev = current
-            current = current.next
-        if current != None:
-            prev.next = current.next
-            del current
-            print('value', value, 'deleted')
+    def delVal(self, value):
+        if self.head is None:
+            print('Empty List')
+            return
+        if self.head.data == value:
+            temp = self.head
+            self.head = self.head.next
+            del temp
+            print('Value Deleted')
         else:
-            print("value not deleted")
+            current = self.head
+            prev = None
+            while current != None and current.data != value:
+                prev = current
+                current = current.next
+            if current != None:
+                prev.next = current.next
+                del current
+                print('value', value, 'deleted')
+            else:
+                print("value not deleted")
 
--- python/foundations/LinkedList/stack_implementation_using_linked_list/stack_implementation.py
+++ python/foundations/LinkedList/stack_implementation_using_linked_list/stack_implementation.py
@@ -16,4 +16,71 @@ class LinkedStack:
             self.top = temp
 
     def pop(self):
-        if 
+        if self.top is None: # Empty List
+            print("Stack Underflow")
+            return None
+        else:
+            temp = self.top
+            value = self.top.data
+            self.top = self.top.next
+            del temp
+            return value
+        
+    def isEmpty(self):
+        return self.top is None
+    
+    def getTop(self):
+        if not(self.isEmpty()):
+            return self.top.data
+        else:
+            print("stack empty")
+            return None
+    
+    def __str__(self):
+        temp = self.top
+        result = ""
+        if temp != None:
+            while temp.next != None:
+                result += str(temp.data) + "->"
+                temp = temp.next
+            result += str(temp.data)
+        else:
+            result = "empty stack"
+        return result
+    
+
+def main():
+    while 1:
+        print("1: create Stack")
+        print("2: Delete Stack")
+        print("3: push")
+        print("4: Pop")
+        print("5: Print Stack Data")
+        print("6: Top Element")
+        choice = int(input("Enter choice : "))
+        if choice == 1:
+            stk = LinkedStack()
+            print("Stack Created")
+        elif choice == 2:
+            del stk
+            print("Stack deleted")
+        elif choice == 3:
+            element = int(input("Enter integer value to be pushed "))
+            stk.push(element)
+            print("Value Added")
+        elif choice == 4:
+            value = stk.pop()
+            print("Element popped : ", value)
+        elif choice == 5:
+            print(str(stk))
+        elif choice == 6:
+            value = stk.getTop()
+            print("Top Element ", value)
+        proceed = input("Enter y if you wish to continue")
+        if proceed != 'y' and proceed != "Y":
+            break
+if __name__ == "__main__":
+    main()
+
+        
+    
