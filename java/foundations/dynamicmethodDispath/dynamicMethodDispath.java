fcae7f526c66a49edd15707f1d1b939916134672
RyzenMu <muruga13in2022@gmail.com>
2023-04-19 04:33:52 +0530

    dynamic method dispatch - java

--- java/foundations/dynamicmethodDispath/dynamicMethodDispath.java
+++ java/foundations/dynamicmethodDispath/dynamicMethodDispath.java
@@ -3,5 +3,7 @@ package java.foundations.dynamicmethodDispath;
 public class dynamicMethodDispath {
     public static void main(String args[]){
         System.out.println("Dynamic Method Dispath");
+        System.out.println("Dynamic Method Dispath");
+
     }
 }
