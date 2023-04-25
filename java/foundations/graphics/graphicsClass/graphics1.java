0a64410741ee177b4a79488cdfc1e67e23ae87ee
RyzenMu <muruga13in2022@gmail.com>
2023-04-26 04:37:52 +0530

    java awt graphics - java

---
+++ java/foundations/graphics/graphicsClass/graphics1.java
@@ -0,0 +1,21 @@
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+
+public class graphics1 extends Applet {
+    String s = new String();
+    String s1 = new String();
+    String s2 = new String();
+    Font f1 = new Font("Courier New", Font.BOLD, 20);
+    public void paint (Graphics GA){
+        GA.setFont(f1);
+        GA.setColor(Color.blue);
+        GA.drawString("Illustration of methods of Graphics class", 200, 520);
+        Font f2 = GA.getFont();
+        s = f2.toString();
+        GA.drawString(s2, 5, 560);
+
+    
+    }
+}
---
+++ java/foundations/graphics/graphicsClass/graphicsClass.java
@@ -0,0 +1,33 @@
+
+public class graphicsClass {
+      public static void main(String args[]){
+        /*
+         * java applets can draw lines
+         * every applet has its own area of the screen known as canvas
+         * the size of the applet is decided by the attributes in <APPLET...>
+         * images are drawn using coordinates (0, 0)
+         * (0, 0) is the top left corner - 3rd quarter
+         * x, y are in pixels
+         * MOSTLY USED GRAPHICS CLASS
+         * clearRect()- erases a rectangular area of the canvas
+         * copyArea()- copies a rectangular area
+         * drawArc()- draws a hollow arc
+         * drawLine() - draws a straight line
+         * drawOval() - draws a hollow oval
+         * drawPolygon() - draws a hollow polygon
+         * drawRect() - draws a hollow rectangle
+         * drawRoundRect() - draws a hollow rectangle with rounded corners
+         * drawString() - Displays a text String
+         * fillArc() - Draws a filled Arc
+         * fillOval() - Draws a filled Oval
+         * fillPolygon() - Draws a filled polygon
+         * fillReact() - Draws a filled rectangle
+         * fillRoundReact()
+         * getColor() - retrives the current drawing color
+         * getFont() - Retrives the current used font
+         * getFontMetrics() - retirives information about the current font
+         * setColor() - sets the drawing color
+         * setFont() - sets the font
+         */
+      }  
+}
