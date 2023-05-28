f08e1d61653b8efc59ab9a572b7581869c7ea3c6
RyzenMu <muruga13in2022@gmail.com>
2023-05-28 06:16:08 +0530

    AWT - ja

---
+++ java/foundations/graphics/AWT/appletCode.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.awt.*;
+import java.applet.*;
+import java.applet.*;
+
+
+public class appletCode extends Applet {
+
+    public void paint(Graphics g){
+        g.drawString("Tutor Joes", 10, 100);
+        // <Applet code = "appletCode.class" width="400" height="400"></Applet>
+    }
+
+  
+    
+    
+}
--- java/foundations/graphics/graphicsClass/graphics2.java
+++ java/foundations/graphics/graphicsClass/graphics2.java
@@ -2,6 +2,6 @@
 
 public class graphics2 {
     public static void main(String args[]){
-        System.out.println("Hello");
+        System.out.println("Hello AWT");
     }    
 }
