1804c469c06fcb6b2a551666efe2e72bb9cba129
RyzenMu <muruga13in2022@gmail.com>
2023-05-31 06:20:04 +0530

    removing elements from array - js

---
+++ java/foundations/mathematicalFunctions/example1.java
@@ -0,0 +1,30 @@
+
+public class example1 {
+    public static void main(String args[]){
+        /*
+         * sin
+         * cos
+         * tan
+         * asin
+         * acos
+         * atan
+         * atan2
+         * pow
+         * exp
+         * log
+         * sqrt
+         * ceil
+         * floor
+         * rint
+         * round
+         * abs
+         * max(a, b)
+         * min(a, b)
+         */
+        double p = Math.pow(3, 4);
+        System.out.println(p);
+
+        double e = Math.exp(p);
+        System.out.println("The exponent value is " + e);
+    }
+}
