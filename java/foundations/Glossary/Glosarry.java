c2529687df0840c892ba9dfab0adcbd396dc5f33
RyzenMu <106828992+RyzenMu@users.noreply.github.com>
2023-08-26 05:53:58 +0530

    Update Glosarry.java

--- java/foundations/Glossary/Glosarry.java
+++ java/foundations/Glossary/Glosarry.java
@@ -8,7 +8,7 @@ public class Glosarry {
          * AWT - Abstract Windowing Toolkit
          * canvas - an applet component that can display graphics and text
          * constructor - a method that is used to create an instantiation of a class
-         * deadlock - Deadlock occurs when two or more threads are awaiting for resources that they can not get.
+         * deadlock - Deadlock occurs when two or more threads are awaiting for resources that they can not get..
          * dynamic linking - when function called within a program are associated with the program at runtime.
          * encapsulation
          * final - a modifier that prevents subclass definition, makes variables constant, and prevents a subclass from overriding a method.
